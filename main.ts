efaefce9b3b9df4a65357527ece709b50a85310a
athenaaa-rgnier <125866234+athenaaa-rgnier@users.noreply.github.com>
2023-02-20 16:52:10 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,16 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
+input.onButtonPressed(Button.A, function () {
     if (lightspot.get(LedSpriteProperty.X) == 2) {
         game.addScore(1)
         time_interval = time_interval - 50
     } else {
         game.gameOver()
     }
-    
 })
 let time_interval = 0
-let lightspot : game.LedSprite = null
+let lightspot: game.LedSprite = null
 lightspot = game.createSprite(2, 2)
 time_interval = 600
-basic.forever(function on_forever() {
+basic.forever(function () {
     lightspot.move(1)
     lightspot.ifOnEdgeBounce()
     basic.pause(time_interval)
